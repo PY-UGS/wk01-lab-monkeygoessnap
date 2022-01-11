52638b868d22c5fa2be737f21c361a0edb1482b3
John W <77936767+monkeygoessnap@users.noreply.github.com>
2022-01-11 16:15:00 +0800

    Add files via upload

---
+++ Question2a.java
@@ -0,0 +1,7 @@
+public class Question2a 
+{   
+    public static void main(String arg[])   
+    {   
+        System.out.println("Hello, I am John!");
+    }   
+}  
---
+++ Question2c.java
@@ -0,0 +1,11 @@
+public class Question2c 
+{   
+    public static void main(String arg[])   
+    {   
+        for(int x=102;x>=66;x-=1) {
+            if (x%2==1) {
+                System.out.println("value of x : " + x);
+            }
+        }       
+    }
+}
