647b9938ab30f1786488416c78477f6695467d61
John W <77936767+monkeygoessnap@users.noreply.github.com>
2022-01-25 10:11:33 +0800

    Update Question2c.java

--- Question2c.java
+++ Question2c.java
@@ -2,7 +2,7 @@ public class Question2c
 {   
     public static void main(String arg[])   
     {   
-        for(int x=102;x>=66;x-=1) {
+        for(int x=102;x>=66;x--) {
             if (x%2==1) {
                 System.out.println("value of x : " + x);
             }
